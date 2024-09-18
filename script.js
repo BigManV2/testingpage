6f642b8c78e50704da78a3b4f0ae303c25055c6a
BigManV2 <132415649+BigManV2@users.noreply.github.com>
2024-09-18 12:04:39 -0400

    commit

--- script.js
+++ script.js
@@ -4,14 +4,6 @@ function getURLParameter(name) {
     return urlParams.get(name);
 }
 
-// Change the text based on the URL parameter
-function changeText() {
-    const newText = getURLParameter('text');
-    if (newText) {
-        document.getElementById('dynamicText').innerText = newText;
-    }
-}
-
 // Change the title based on the URL parameter
 function changeTitle() {
     const newTitle = getURLParameter('title');
@@ -20,9 +12,46 @@ function changeTitle() {
     }
 }
 
-// Call the function when the page loads
-window.onload = function() {
-    changeTitle();
-    changeText();
-};
-//frf
+// Change Open Graph title based on URL parameter
+function changeOgTitle() {
+    const newOgTitle = getURLParameter('ogTitle');
+    if (newOgTitle) {
+        const metaOgTitle = document.querySelector('meta[property="og:title"]');
+        if (metaOgTitle) {
+            metaOgTitle.setAttribute('content', newOgTitle);
+        }
+    }
+}
+
+// Change Open Graph description based on URL parameter
+function changeOgDescription() {
+    const newOgDescription = getURLParameter('ogDescription');
+    if (newOgDescription) {
+        const metaOgDescription = document.querySelector('meta[property="og:description"]');
+        if (metaOgDescription) {
+            metaOgDescription.setAttribute('content', newOgDescription);
+        }
+    }
+}
+
+// Change Open Graph image based on URL parameter
+function changeOgImage() {
+    const newOgImage = getURLParameter('ogImage');
+    if (newOgImage) {
+        const metaOgImage = document.querySelector('meta[property="og:image"]');
+        if (metaOgImage) {
+            metaOgImage.setAttribute('content', newOgImage);
+        }
+    }
+}
+
+// Call all functions on page load
+function init() {
+    changeTitle(); 
+    changeOgTitle();
+    changeOgDescription();
+    changeOgImage();
+}
+
+// Assign the init function to window.onload
+window.onload = init;
