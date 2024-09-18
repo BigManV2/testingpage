b833fb82fc58af7048dc8bab3199b38a2fca905a
BigManV2 <132415649+BigManV2@users.noreply.github.com>
2024-09-18 10:47:16 -0400

    commit

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+// Function to get URL parameters
+function getURLParameter(name) {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get(name);
+}
+
+// Change the text based on the URL parameter
+function changeText() {
+    const newText = getURLParameter('text');
+    if (newText) {
+        document.getElementById('dynamicText').innerText = newText;
+    }
+}
+
+// Call the function when the page loads
+window.onload = changeText;
 
